1e4fbf0a4d04646986538c6c88c51171a9fab08a
Sarathi S <124993897+WebRevo@users.noreply.github.com>
2024-12-21 19:49:57 +0530

    initial commit

---
+++ src/movement/tadMovement.js
@@ -0,0 +1,24 @@
+/*
+ * TADMOVEMENT
+ * All of the movement patterns for tadpoles are contained here
+ */
+
+import Movement from './movement'
+
+class TadMovement {
+  constructor(entity, canvas, collisions) {
+    this.canvas = canvas;
+    this.boids = entity;
+    this.collisions = collisions;
+  }
+
+  move() {
+    this.boids.forEach((boid) => {
+      boid.flock(this.boids);
+      boid.flee(this.collisions.fish);
+      this.collisions.checkTadpoles(this);
+    });
+  }
+}
+
+export default TadMovement;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/pond.js',
+  output: {
+    path: __dirname + '/docs/',
+    filename: './js/pond.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              // options...
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['docs'] },
+      files: ['./docs/*'],
+      notify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+	new MiniCssExtractPlugin({
+      filename: 'css/[name].bundle.css'
+    })
+  ],
+  watch: true,
+  devtool: 'source-map'
+}
